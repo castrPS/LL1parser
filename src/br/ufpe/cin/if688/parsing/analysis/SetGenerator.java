807e61d862223c6b9044a2b6c0665853fc571b7a
Rafael Prado Santos <rps4@WINDOWS.CIN.UFPE.BR>
2018-04-09 16:09:48 -0300

    Add o follow que faltava

--- src/br/ufpe/cin/if688/parsing/analysis/SetGenerator.java
+++ src/br/ufpe/cin/if688/parsing/analysis/SetGenerator.java
@@ -12,9 +12,6 @@ public final class SetGenerator {
     	if (g == null) throw new NullPointerException("g nao pode ser nula.");
         
     	Map<Nonterminal, Set<GeneralSymbol>> first = initializeNonterminalMapping(g);
-    	/*
-    	 * Implemente aqui o método para retornar o conjunto first
-    	 */
     	
     	Collection <Production> productions = g.getProductions();
  		Nonterminal nonTerminal = null;
@@ -28,39 +25,12 @@ public final class SetGenerator {
 
 
  		}
+
         return first;
     	
     }
 
     
-    public static Map<Nonterminal, Set<GeneralSymbol>> getFollow(Grammar g, Map<Nonterminal, Set<GeneralSymbol>> first) {
-        
-        if (g == null || first == null)
-            throw new NullPointerException();
-                
-        Map<Nonterminal, Set<GeneralSymbol>> follow = initializeNonterminalMapping(g);
-        
-        /*
-         * implemente aqui o método para retornar o conjunto follow
-         */
-        
-       
-        
-        return follow;
-    }
-    
-    //método para inicializar mapeamento nãoterminais -> conjunto de símbolos
-    private static Map<Nonterminal, Set<GeneralSymbol>>
-    initializeNonterminalMapping(Grammar g) {
-    Map<Nonterminal, Set<GeneralSymbol>> result = 
-        new HashMap<Nonterminal, Set<GeneralSymbol>>();
-
-    for (Nonterminal nt: g.getNonterminals())
-        result.put(nt, new HashSet<GeneralSymbol>());
-
-    return result;
-}
-
     public static Set <GeneralSymbol> getFirstSymbol (Set <GeneralSymbol> aux, Collection <Production> productions, GeneralSymbol symbol){
 		List <GeneralSymbol> current = null;
 		List<Production> p_symbol = new ArrayList<Production>();
@@ -103,4 +73,75 @@ public final class SetGenerator {
 		return aux; 
 		
 	}
+    
+    public static Map<Nonterminal, Set<GeneralSymbol>> getFollow(Grammar g, Map<Nonterminal, Set<GeneralSymbol>> first) {
+        
+        if (g == null || first == null)
+            throw new NullPointerException();
+                
+        Map<Nonterminal, Set<GeneralSymbol>> follow = initializeNonterminalMapping(g);
+        
+        Map<Nonterminal, Set<GeneralSymbol>> follow_aux;
+        Collection <Production> productions = g.getProductions();
+		GeneralSymbol start = g.getStartSymbol();
+		GeneralSymbol current = null;
+		GeneralSymbol current_next = null;
+		Set<GeneralSymbol> set_symb;
+		Set<GeneralSymbol> bi;
+		follow.get(start).add(SpecialSymbol.EOF);
+		boolean change = true;
+		
+		while(change) {
+			follow_aux = new HashMap<Nonterminal, Set<GeneralSymbol>>();
+			for(Nonterminal nt:follow.keySet()) {
+				set_symb = new HashSet<GeneralSymbol>();
+				set_symb.addAll(follow.get(nt));
+				follow_aux.put(nt, set_symb);
+			}
+			//baseado no algoritmo do livro  ENGINEERING A COMPILER 
+			for (Production p : productions)  {
+				set_symb = new HashSet<GeneralSymbol>();
+				set_symb.addAll(follow.get(p.getNonterminal()));
+				for(int i = p.getProduction().size()-1; i>=0;i-- ) {
+					if(p.getProduction().get(i) instanceof Nonterminal) {
+						follow.get(p.getProduction().get(i)).addAll(set_symb);
+						if(first.get(p.getProduction().get(i)).contains(SpecialSymbol.EPSILON)) {
+							bi = new HashSet<GeneralSymbol>();
+							bi.addAll(first.get(p.getProduction().get(i)));
+							bi.remove(SpecialSymbol.EPSILON);
+							set_symb.addAll(bi);
+						}else {
+							set_symb = new HashSet<GeneralSymbol>();
+							set_symb.addAll(first.get(p.getProduction().get(i)));
+						}
+					}else{
+						set_symb = new HashSet<GeneralSymbol>();
+						set_symb.add(p.getProduction().get(i));
+						set_symb.addAll(set_symb);
+					}
+				
+				}
+				
+				change = !follow.equals(follow_aux);
+			
+			}
+			
+		}
+	
+        
+        return follow;
+    }
+    
+    //método para inicializar mapeamento nãoterminais -> conjunto de símbolos
+    private static Map<Nonterminal, Set<GeneralSymbol>>
+    initializeNonterminalMapping(Grammar g) {
+    Map<Nonterminal, Set<GeneralSymbol>> result = 
+        new HashMap<Nonterminal, Set<GeneralSymbol>>();
+
+    for (Nonterminal nt: g.getNonterminals())
+        result.put(nt, new HashSet<GeneralSymbol>());
+
+    return result;
 }
+
+} 
--- src/br/ufpe/cin/if688/table/Table.java
+++ src/br/ufpe/cin/if688/table/Table.java
@@ -20,9 +20,34 @@ public final class Table {
         Map<LL1Key, List<GeneralSymbol>> parsingTable = 
             new HashMap<LL1Key, List<GeneralSymbol>>();
 
-        /*
-         * Implemente aqui o método para retornar a parsing table
-         */
+        Collection<Production> productions = g.getProductions();
+        Iterator<GeneralSymbol> t_first, t_follow;               //Iterator para First e do Follow
+        GeneralSymbol s_first, s_follow;                         //Simbolos de First e Follow
+        LL1Key key;
+        
+        for (Production production : productions) {
+			t_first = first.get(production.getNonterminal()).iterator();
+			
+			while(t_first.hasNext()) {
+				 s_first = t_first.next();
+				 
+	     		   if(!s_first.equals(SpecialSymbol.EPSILON) && !production.getProduction().contains(SpecialSymbol.EPSILON)) {
+	     			   
+	     			   key = new LL1Key(production.getNonterminal(), s_first);  //Para todo a ∈ FIRST(α), adicione A → α em M[A,a]
+	         		   parsingTable.put(key, production.getProduction());
+	         		  
+	         		   
+	     		   } else if(s_first.equals(SpecialSymbol.EPSILON) && production.getProduction().contains(SpecialSymbol.EPSILON)) {
+	     			  t_follow = follow.get(production.getNonterminal()).iterator();
+	     			   while(t_follow.hasNext()) {                                     //Se ε ∈ FIRST(α), então, para todo b ∈FOLLOW(A), adicione A → α em M[A,b]
+	     				   s_follow = t_follow.next();
+	     				   key = new LL1Key(production.getNonterminal(), s_follow);
+	     				   System.out.println(key);
+	     				   parsingTable.put(key, production.getProduction());
+	     			   }
+	     		   } 
+	     	   }
+			}
         
         return parsingTable;
     }
